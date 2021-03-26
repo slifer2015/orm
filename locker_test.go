5dac896c2087fe927750163808903079ad902a5b
Lukasz Lato <lukasz.lato@summer-solutions.com>
2021-03-26 22:33:49 +0100

    redis search documentation

--- locker_test.go
+++ locker_test.go
@@ -1,6 +1,7 @@
 package orm
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -25,6 +26,8 @@ func TestLocker(t *testing.T) {
 	lock, has := l.Obtain(engine.context, "test_key", time.Second, 0)
 	assert.True(t, has)
 	assert.NotNil(t, lock)
+	has = lock.Refresh(engine.context, time.Second)
+	assert.True(t, has)
 
 	_, has = l.Obtain(engine.context, "test_key", time.Second, time.Millisecond)
 	assert.False(t, has)
@@ -34,12 +37,33 @@ func TestLocker(t *testing.T) {
 
 	lock.Release()
 	lock.Release()
+	has = lock.Refresh(engine.context, time.Second)
+	assert.False(t, has)
+	lock.has = true
+	lock.done = make(chan bool)
+	lock.Release()
 	_, has = l.Obtain(engine.context, "test_key", time.Second, time.Millisecond)
 	assert.True(t, has)
+	lock.has = true
+	lock.done = make(chan bool)
+	has = lock.Refresh(engine.context, time.Second)
+	assert.False(t, has)
 
 	assert.PanicsWithError(t, "ttl must be higher than zero", func() {
 		_, _ = l.Obtain(engine.context, "test_key", 0, time.Millisecond)
 	})
+
+	lock, has = l.Obtain(engine.context, "test_key_2", time.Millisecond*3, 0)
+	assert.True(t, has)
+	time.Sleep(time.Millisecond * 4)
+	assert.Equal(t, time.Duration(0), lock.TTL())
+
+	ctxCancel, cancel := context.WithCancel(engine.context)
+	lock, has = l.Obtain(ctxCancel, "test_key_3", time.Millisecond*3, 0)
+	assert.True(t, has)
+	cancel()
+	time.Sleep(time.Millisecond)
+	assert.Equal(t, time.Duration(0), lock.TTL())
 
 	registry = &Registry{}
 	registry.RegisterRedis("localhost:6389", 15)
